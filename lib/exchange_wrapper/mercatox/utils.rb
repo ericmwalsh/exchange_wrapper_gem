80ab055ebb658213d82ebbf712e3023d435e4772
Eric Walsh <ericmatthewwalsh@gmail.com>
2018-03-17 22:37:08 -0700

    add mercatox #volume method

--- lib/exchange_wrapper/mercatox/utils.rb
+++ lib/exchange_wrapper/mercatox/utils.rb
@@ -24,14 +24,11 @@ module ExchangeWrapper
         def symbols
           symbols = []
 
-          fetch_prices.each do |tp, symbol_hash|
-            next unless tp.present? && symbol_hash['isFrozen'] == "0"
-            assets = tp.split('_')
-            base_asset = assets[0]
-            quote_asset = assets[1]
+          fetch_metadata.each do |md_hash|
+            assets = md_hash['symbol'].split('/')
 
-            symbols << base_asset if base_asset.present?
-            symbols << quote_asset if quote_asset.present?
+            symbols << assets[0]
+            symbols << assets[1]
           end
 
           symbols.sort!.uniq!
@@ -42,17 +39,12 @@ module ExchangeWrapper
         def trading_pairs
           trading_pairs = []
 
-          fetch_prices.each do |tp, symbol_hash|
-            next unless tp.present? && symbol_hash['isFrozen'] == "0"
-            assets = tp.split('_')
-            base_asset = assets[0]
-            quote_asset = assets[1]
-            next unless base_asset.present? && quote_asset.present?
-
+          fetch_metadata.each do |md_hash|
+            assets = md_hash['symbol'].split('/')
             trading_pairs << [
-              "#{base_asset}/#{quote_asset}",
-              base_asset,
-              quote_asset
+              md_hash['symbol'],
+              assets[0],
+              assets[1]
             ]
           end
 
@@ -65,16 +57,12 @@ module ExchangeWrapper
         def prices
           prices = []
 
-          fetch_prices.each do |tp, tp_hash|
-            next unless tp.present? && tp_hash['isFrozen'] == "0" && tp_hash['last'].present?
-            assets = tp.split('_')
-            base_asset = assets[0]
-            quote_asset = assets[1]
-            next unless base_asset.present? && quote_asset.present?
+          fetch_metadata.each do |md_hash|
+            next unless md_hash['last'].present?
 
             prices << {
-              'symbol' => "#{assets[0]}/#{assets[1]}",
-              'price' => tp_hash['last']
+              'symbol' => md_hash['symbol'],
+              'price' => md_hash['last']
             }
           end
 
@@ -82,31 +70,47 @@ module ExchangeWrapper
         end
 
         def metadata
-          metadata = []
+          fetch_metadata
+        end
 
-          fetch_prices.each do |tp, tp_hash|
-            next unless tp.present? && tp_hash['isFrozen'] == "0"
-            assets = tp.split('_')
-            base_asset = assets[0]
-            quote_asset = assets[1]
-            next unless base_asset.present? && quote_asset.present?
+        def volume
+          volume = []
 
-            metadata << tp_hash.merge('symbol' => "#{assets[0]}/#{assets[1]}")
+          fetch_metadata.each do |md_hash|
+            next unless md_hash['baseVolume'].present? && md_hash['quoteVolume'].present?
+
+            volume << {
+              'symbol' => md_hash['symbol'],
+              'base_volume' => md_hash['baseVolume'],
+              'quote_volume' => md_hash['quoteVolume']
+            }
           end
 
-          metadata
+          volume
         end
 
         private
 
-        def fetch_prices
+        def fetch_metadata
+          metadata = []
+
           if defined?(::Rails)
             ::Rails.cache.fetch('ExchangeWrapper/mercatox-public-api-market', expires_in: 30.seconds) do
               ::ExchangeWrapper::Mercatox::PublicApi.market
             end
           else
             ::ExchangeWrapper::Mercatox::PublicApi.market
-          end['pairs']
+          end['pairs'].each do |tp, md_hash|
+            next unless tp.present? && md_hash['isFrozen'] == "0"
+            assets = tp.to_s.split('_')
+            base_asset = assets[0]
+            quote_asset = assets[1]
+            next unless base_asset.present? && quote_asset.present?
+
+            metadata << md_hash.merge('symbol' => "#{assets[0]}/#{assets[1]}")
+          end
+
+          metadata
         end
 
       end
